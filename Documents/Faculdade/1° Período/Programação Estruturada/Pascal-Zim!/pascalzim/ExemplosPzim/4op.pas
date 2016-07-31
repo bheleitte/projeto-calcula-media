8150c52d4d210b2800e5068e85aa3e682cb6d8b8
bheleitte <brendaleite7714@gmail.com>
2016-07-31 14:26:00 -0300

    primeiro commit

---
+++ Documents/Faculdade/1° Período/Programação Estruturada/Pascal-Zim!/pascalzim/ExemplosPzim/4op.pas
@@ -0,0 +1,13 @@
+Program Pzim ;
+var
+a,b,soma,sub,mult,dive:integer;
+ Begin
+   writeln('digite o 1 numeros');
+   readln(a);
+   writeln('digite o 2 numeros');
+   readln(b);
+   soma:=a+b;
+   sub:=a-b;
+   mult:=a*b;
+   dive:=a/b;
+ End.
